f2d6c31cfb2c1c832bdda2974587d998f8ef9873
Jason Barrett <jason.barrett.55@gmail.com>
2017-12-04 11:13:15 +0000

    Added CreditCard class

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -1,2 +1,47 @@
-public class CreditCard {
+public class CreditCard
+{
+    private int cardNo;
+    private long balance;
+    private String expiryDate;
+    private int cvc;
+    private int limit;
+    int rewardPoints;
+    private CardMember member;
+    private CardIssuer issuer;
+
+    public CreditCard(int cardNo, long balance, String expiryDate, int cvc, int limit, int rewardPoints, CardMember member, CardIssuer issuer)
+    {
+        this.cardNo = cardNo;
+        this.expiryDate = expiryDate;
+        this.cvc = cvc;
+        this.limit = limit;
+        this.rewardPoints = rewardPoints;
+        this.member = member;
+        this.issuer = issuer;
+    }
+
+
+    public int getCardNo() {
+        return cardNo;
+    }
+
+    public String getExpiryDate() {
+        return expiryDate;
+    }
+
+    public int getCvc() {
+        return cvc;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public CardMember getMember() {
+        return member;
+    }
+
+    public CardIssuer getIssuer() {
+        return issuer;
+    }
 }
