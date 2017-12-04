b87e7e2fabf724eaff79f1a8a9de070f3fdeb874
ArthurYe97 <slnditftw@gmail.com>
2017-12-04 10:55:28 +0000

    Add files via upload

---
+++ src/CardMember.java
@@ -0,0 +1,94 @@
+
+public class CardMember
+{
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String address;
+	private String city;
+	private String postCode;
+	private String country;
+	private String phoneNumber;
+	private String id;
+	
+	public void setFirstName(String firstName)
+	{
+		this.firstName = firstName;
+	}
+	public String getFirstName()
+	{
+		return firstName;
+	}
+	
+	public void setLastName(String lastName)
+	{
+		this.lastName = lastName;
+	}
+	public String getLastName()
+	{
+		return lastName;
+	}
+	
+	public void setEmail(String email)
+	{
+		this.email = email;
+	}
+	public String getEmail()
+	{
+		return email;
+	}
+	
+	public void setAddress(String address)
+	{
+		this.address = address;
+	}
+	public String getAddress()
+	{
+		return address;
+	}
+	
+	public void setCity(String city)
+	{
+		this.city = city;
+	}
+	public String getCity()
+	{
+		return city;
+	}
+	
+	public void setPostCode(String postCode)
+	{
+		this.postCode = postCode;
+	}
+	public String getPostCode() {
+		return postCode;
+	}
+	
+	public void setCountry(String country)
+	{
+		this.country = country;
+	}
+	public String getCountry()
+	{
+		return country;
+	}
+	
+	public void setPhoneNumber(String phoneNumber)
+	{
+		this.phoneNumber = phoneNumber;
+	}
+	public String getPhoneNumber()
+	{
+		return phoneNumber;
+	}
+	
+	public void setID(String id)
+	{
+		this.id = id;
+	}
+	public String getId()
+	{
+		return id;
+	}
+}
+
