60415712155cf66c42a26446d7fd936922b3bba4
Kiril Kostadinov <kiril.a.kostadinov@abv.bg>
2017-12-04 10:43:27 +0000

    Update CardIssuer.java

--- src/CardIssuer.java
+++ src/CardIssuer.java
@@ -1,2 +1,22 @@
+package amex.challenge;
+
 public class CardIssuer {
+	private int id;
+	private long balance;
+	
+	public CardIssuer(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	public long getBalance() {
+		return balance;
+	}
+	
+	public void setBalance(long balance) {
+		this.balance = balance;
+	}
 }
